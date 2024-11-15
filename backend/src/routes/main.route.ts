18d13fe72bd5238720ef1c3a7a0809f8d6420d80
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-11-15 15:47:09 -0300

    feat: Created the main route

---
+++ backend/src/routes/main.route.ts
@@ -0,0 +1,13 @@
+import { Request, Response, Router } from 'express';
+
+import packageJson from '../../package.json';
+
+const mainRoute = Router();
+
+mainRoute.get('/', (req: Request, res: Response) => {
+  const { name, description, author } = packageJson;
+
+  res.status(200).json({ name, description, author });
+});
+
+export default mainRoute;
