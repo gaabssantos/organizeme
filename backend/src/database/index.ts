8ecc25d0e1a8fcdb2e70196524201ab4120670fb
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-11-16 12:11:18 -0300

    feat: Connecting to database

---
+++ backend/src/database/index.ts
@@ -0,0 +1,22 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DATABASE as string,
+  process.env.POSTGRES_USERNAME as string,
+  process.env.POSTGRES_PASSWORD as string,
+  {
+    host: process.env.POSTGRES_HOST as string,
+    dialect: 'postgres',
+  },
+);
+
+const setupDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✔ Connection has been established successfully.');
+  } catch (err) {
+    console.error('❌ Unable to connect the database, error: ', err);
+  }
+};
+
+export default setupDatabase;
