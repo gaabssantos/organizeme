8755f9eaac20bad184031bd4390258615cee3adf
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-11-17 13:26:21 -0300

    feat: Created a validation for board form

--- backend/src/dtos/board.dto.ts
+++ backend/src/dtos/board.dto.ts
@@ -1,5 +1,20 @@
+import z from 'zod';
+
 export type BoardDTO = {
   id_user: string;
   name: string;
   color: string;
+};
+
+export const boardSchema = {
+  name: z
+    .string()
+    .trim()
+    .min(5, { message: 'The name must have 5 characters.' }),
+  color: z
+    .string()
+    .refine(
+      (value) => /^#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$/.test(value ?? ''),
+      'The color should be a hex color.',
+    ),
 };
--- backend/src/routes/board.route.ts
+++ backend/src/routes/board.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
 import { BoardController } from '../controllers/board.controller';
+import { boardSchema } from '../dtos/board.dto';
 import { BoardFactory } from '../factories/board.factory';
 import authMiddleware from '../middlewares/auth.middleware';
+import { ParamsType, validator } from '../middlewares/validator.middleware';
 
 const board = Router();
 
@@ -10,7 +12,11 @@ const controller = new BoardController(BoardFactory.getServiceInstance());
 
 board.use(authMiddleware);
 
-board.post('/create', controller.create);
+board.post(
+  '/create',
+  validator({ schema: boardSchema, type: ParamsType.BODY }),
+  controller.create,
+);
 board.get('/', controller.index);
 
 export default board;
