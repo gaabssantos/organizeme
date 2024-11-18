fd1149b507962c38d01569eca0a87b24ffe682a0
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-11-18 20:39:37 -0300

    Created schema for Card

--- backend/src/dtos/card.dto.ts
+++ backend/src/dtos/card.dto.ts
@@ -1,5 +1,19 @@
+import z from 'zod';
+
 export type CardDTO = {
   name: string;
   description: string;
   list_id: string;
+};
+
+export const cardSchema = {
+  name: z
+    .string()
+    .trim()
+    .min(5, { message: 'The name must have 5 or more characters.' }),
+  description: z
+    .string()
+    .trim()
+    .min(10, { message: 'The description must have 10 or more characters.' }),
+  list_id: z.string(),
 };
--- backend/src/routes/card.route.ts
+++ backend/src/routes/card.route.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 
 import { CardController } from '../controllers/card.controller';
+import { cardSchema } from '../dtos/card.dto';
 import { CardFactory } from '../factories/card.factory';
+import { ParamsType, validator } from '../middlewares/validator.middleware';
 
 const card = Router();
 
 const controller = new CardController(CardFactory.getServiceInstance());
 
-card.post('/create', controller.create);
+card.post(
+  '/create',
+  validator({ schema: cardSchema, type: ParamsType.BODY }),
+  controller.create,
+);
 
 export default card;
