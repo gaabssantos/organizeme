5a616f5961c23db3b3f7f3b80def06cd18aa192c
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-11-21 20:19:20 -0300

    feat: Created sidebar

--- frontend/src/screens/Home/home.screen.tsx
+++ frontend/src/screens/Home/home.screen.tsx
@@ -1,9 +1,11 @@
 import Header from '../../components/Header/header.component';
+import Sidebar from '../../components/Sidebar/sidebar.component';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Sidebar />
     </div>
   );
 };
