2f5bd907c3498682054cf93f7c5ba48fa5aa9a1f
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-11-21 20:19:11 -0300

    style: Added color to themes

--- frontend/src/styles/themes.style.ts
+++ frontend/src/styles/themes.style.ts
@@ -1,6 +1,8 @@
 export const themes = {
   navColor: '#1D2125',
+  sideColor: '#221D24',
   textColor: '#B6C2CF',
+  hoverColor: '#333c43',
   primaryColor: '#1275ff',
   black: '#000000',
   white: '#ffffff',
