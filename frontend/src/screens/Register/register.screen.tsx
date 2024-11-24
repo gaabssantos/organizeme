8dbd89e9b7fe133f436ee6ca8ee4ebf286a6c03c
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-11-24 13:25:35 -0300

    refactor: Added formik to forms

--- frontend/src/screens/Register/register.screen.tsx
+++ frontend/src/screens/Register/register.screen.tsx
@@ -1,19 +1,152 @@
+import { useFormik } from 'formik';
+import { withZodSchema } from 'formik-validator-zod';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import z from 'zod';
 
-import { Divisor, LoginRegisterBox } from '../../components/global.component';
+import CardMessage from '../../components/CardMessage/card-message.component';
+import {
+  Divisor,
+  ErrorMessage,
+  LoginRegisterBox,
+} from '../../components/global.component';
 import Header from '../../components/Header/header.component';
+import useRegister from '../../hooks/useRegister';
+
+const RegisterFormSchema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .min(3, { message: 'O nome deve ter, pelo menos, 3 caracteres.' }),
+    email: z.string().email({ message: 'Digite um e-mail válido.' }),
+    password: z
+      .string()
+      .trim()
+      .min(8, { message: 'A senha deve ter, pelo menos, 8 caracteres.' }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'A senhas não coincidem',
+    path: ['confirmPassword'],
+  });
+
+type RegisterFormSchemaType = z.infer<typeof RegisterFormSchema>;
+
+type ResponseType = {
+  errCode: string;
+};
 
 const Register = () => {
+  const [cardMessage, setCardMessage] = useState<{
+    typeMessage: 'error' | 'success' | '';
+    headerMessage: string | boolean;
+    message: string | boolean;
+  }>({
+    typeMessage: '',
+    headerMessage: '',
+    message: '',
+  });
+
+  const formik = useFormik<RegisterFormSchemaType>({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    onSubmit: async (values, actions) => {
+      const response = await useRegister(values);
+
+      if (response.error) {
+        const data = response.error.response?.data as ResponseType;
+
+        setCardMessage({
+          typeMessage: 'error',
+          headerMessage: data.errCode === 'email_exists' && 'E-mail já existe',
+          message:
+            data.errCode === 'email_exists' &&
+            'Este e-mail já existe, tente utilizar outro',
+        });
+      } else {
+        setCardMessage({
+          typeMessage: 'success',
+          headerMessage: 'Conta criada',
+          message:
+            'Sua conta foi criada com sucesso, agora apenas cheque seu e-mail para verificar ela.',
+        });
+      }
+
+      actions.resetForm();
+    },
+    validate: withZodSchema(RegisterFormSchema),
+    validateOnChange: false,
+    validateOnBlur: false,
+  });
+
   return (
     <div>
       <Header />
+
+      {(cardMessage.typeMessage !== '' ||
+        cardMessage.headerMessage !== '' ||
+        cardMessage.message !== '') && (
+        <CardMessage
+          typeMessage={cardMessage.typeMessage}
+          headerMessage={cardMessage.headerMessage}
+          message={cardMessage.message}
+        />
+      )}
+
       <LoginRegisterBox>
         <h2>Registre uma conta</h2>
-        <form>
-          <input type="text" placeholder="Digite seu nome" />
-          <input type="email" placeholder="Digite seu e-mail" />
-          <input type="password" placeholder="Digite sua senha" />
-          <button>Registrar</button>
+        <form onSubmit={formik.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Digite seu nome"
+            {...formik.getFieldProps('name')}
+            autoFocus
+          />
+
+          <input
+            type="text"
+            placeholder="Digite seu e-mail"
+            {...formik.getFieldProps('email')}
+          />
+
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            {...formik.getFieldProps('password')}
+          />
+
+          <input
+            type="password"
+            placeholder="Confirme sua senha"
+            {...formik.getFieldProps('confirmPassword')}
+          />
+
+          {!formik.isValid && (
+            <ErrorMessage>
+              <ul>
+                {formik.errors.name && formik.touched.name && (
+                  <li>{formik.errors.name}</li>
+                )}
+                {formik.errors.email && formik.touched.email && (
+                  <li>{formik.errors.email}</li>
+                )}
+                {formik.errors.password && formik.touched.password && (
+                  <li>{formik.errors.password}</li>
+                )}
+                {formik.errors.confirmPassword &&
+                  formik.touched.confirmPassword && (
+                    <li>{formik.errors.confirmPassword}</li>
+                  )}
+              </ul>
+            </ErrorMessage>
+          )}
+
+          <button type="submit">Registrar</button>
           <Divisor />
           <Link to={'/login'}>Já tem uma conta? Clique para entrar nela</Link>
         </form>
