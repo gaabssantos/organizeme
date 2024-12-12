421975bf077ed2404342fd74d234a48a552b2446
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-12-12 19:40:49 -0300

    refactor: Added Params ID

--- frontend/src/screens/Home/home.screen.tsx
+++ frontend/src/screens/Home/home.screen.tsx
@@ -1,3 +1,5 @@
+import { useParams } from 'react-router-dom';
+
 import Board from '../../components/Board/board.component';
 import { Flex } from '../../components/global.component';
 import Header from '../../components/Header/header.component';
@@ -5,13 +7,15 @@ import Modal from '../../components/Modal/modal.component';
 import Sidebar from '../../components/Sidebar/sidebar.component';
 
 const Home = () => {
+  const params = useParams();
+
   return (
     <div>
       <Header />
       <Modal />
       <Flex>
         <Sidebar />
-        <Board />
+        <Board currentId={params.id} />
       </Flex>
     </div>
   );
