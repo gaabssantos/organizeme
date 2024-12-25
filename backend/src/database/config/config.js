1e946a26a31415b2f6767198fb19fdf1eb2afa23
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-12-25 10:22:56 -0300

    chore: Changes postgre DB

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -5,6 +5,7 @@ const config = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
   host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
   dialect: 'postgres',
 };
 
