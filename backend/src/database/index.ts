f967a89cb8bc0aeadd6050485ab2e7cc0daeba11
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-12-25 09:22:42 -0300

    chore: Postgres SQL

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,12 +1,12 @@
 import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE as string,
-  process.env.POSTGRES_USERNAME as string,
-  process.env.POSTGRES_PASSWORD as string,
+  'postgresql://postgres:organizemepostgres@db.zzufbojwhkhntonfgsxc.supabase.co:5432/organizeme',
   {
-    host: process.env.POSTGRES_HOST as string,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
   },
 );
 
