813bce09c86b47ccc6e1aaa174b8ada3d4e95699
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-12-25 09:49:47 -0300

    chore: Removed config.json and added config.ts

---
+++ backend/src/database/config/config.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+
+const config = {
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  host: process.env.POSTGRES_HOST,
+  dialect: 'postgres',
+};
+
+export default {
+  development: config,
+  test: config,
+  production: config,
+};
